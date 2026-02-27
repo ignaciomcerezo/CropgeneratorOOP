053e57446163f76475f0565b94fd19c7c157778e
Nacho <pambignacio8@gmail.com>
2026-02-27 09:35:07 +0100

    in the process of creating tests for the subgraph generation and improving it.

---
+++ preprocessing/classes/tests/test_AnnotatedPage.py
@@ -0,0 +1,33 @@
+from labelstudio.LabelStudioInterface import LabelStudioInterface
+from preprocessing.classes.AnnotatedPage import AnnotatedPage
+from paths import images_path
+from PIL import Image
+from display import display
+
+LSI = LabelStudioInterface()
+tsk_id = 5
+img_path = images_path / f"{str(tsk_id).rjust(3,"0")}.png"
+tsk = LSI[tsk_id][0]
+Annotated_task_5 = AnnotatedPage(
+    tsk,
+    tsk_id,
+    Image.open(img_path),
+    True,
+)
+
+if __name__ == "__main__":
+    cimg, complete_transcription, _ = Annotated_task_5.cluster_reading_order(Annotated_task_5.graph.keys())
+
+    heu_sindex = lambda text: complete_transcription.index(text)
+
+
+    for image_box in Annotated_task_5.image_boxes.values():
+
+        cimg, ctrans, cindex = Annotated_task_5.cluster_reading_order([image_box.id])
+
+        if cindex != heu_sindex(ctrans):
+            print(f"cindex != hey_sindex @{image_box.id}")
+        if cindex != heu_sindex(image_box.fragment.text):
+            print(f"cindex != hey_sindex @{image_box.id}")
+
+
