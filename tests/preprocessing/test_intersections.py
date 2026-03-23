1d5332b7405b597e9c95da8c43e0c8fd5aa8114b
Nacho <pambignacio8@gmail.com>
2026-03-23 17:27:29 +0100

    Finished conversion of tests into PathBundle and new LSI

--- tests/preprocessing/test_intersections.py
+++ tests/preprocessing/test_intersections.py
@@ -1,23 +1,27 @@
 from tests.tests_helper import load_particular_annotation
+from kaggle_integration.PathBundle import PathBundle
 
 
-def test_intersections():
-    ann5 = load_particular_annotation(5, 0)
-    A = ann5.image_boxes["5LKgXZs7ij"]
-    B = ann5.image_boxes["cf7ItU794h"]
-    C = ann5.image_boxes["cXXa92pGsI"]
+def test_intersections_and_geometries():
+    paths = PathBundle()
+    ann5 = load_particular_annotation(paths, 5, 0)
+    box_a = ann5.image_boxes["5LKgXZs7ij"]
+    box_b = ann5.image_boxes["cf7ItU794h"]
+    box_c = ann5.image_boxes["cXXa92pGsI"]
 
-    assert len(A.polygon.exterior.coords[:]) > 4
-    assert len(B.polygon.exterior.coords[:]) > 4
-    assert len(C.polygon.exterior.coords[:]) > 4
+    assert len(box_a.polygon.exterior.coords[:]) > 4
+    assert len(box_b.polygon.exterior.coords[:]) > 4
+    assert len(box_c.polygon.exterior.coords[:]) > 4
 
-    assert A.polygon.intersects(B.polygon)
-    assert B.polygon.intersects(C.polygon)
-    assert not C.polygon.intersects(A.polygon)
+    assert not box_a.true_rectangle
+    assert not box_b.true_rectangle
+    assert not box_c.true_rectangle
 
-    ann5 = load_particular_annotation(344, 1)
-    assert len(ann5.paragraphs) == 2
+    assert box_a.polygon.intersects(box_b.polygon)
+    assert box_b.polygon.intersects(box_c.polygon)
+    assert not box_c.polygon.intersects(box_a.polygon)
 
-    assert len(load_particular_annotation(341, 0).paragraphs) == 1
-    assert len(load_particular_annotation(342, 0).paragraphs) == 1
-    assert len(load_particular_annotation(343, 0).paragraphs) == 2
+    assert len(load_particular_annotation(paths, 341, 0).paragraphs) == 1
+    assert len(load_particular_annotation(paths, 342, 0).paragraphs) == 1
+    assert len(load_particular_annotation(paths, 343, 0).paragraphs) == 2
+    assert len(load_particular_annotation(paths, 344, 1).paragraphs) == 2
