fa4a42968e340de2ff0588e1506c779bd4f09f75
Nacho <pambignacio8@gmail.com>
2026-03-24 20:11:22 +0100

    Changed file structure into a module to ease use in Kaggle

---
+++ cropgen/processing/parallel/augment_data_parallel.py
@@ -0,0 +1,68 @@
+from multiprocessing import Pool, cpu_count
+import numpy as np
+from functools import partial
+from cropgen.shared.PathBundle import PathBundle
+from cropgen.external_interfaces.LabelStudioInterface import LabelStudioInterface
+from cropgen.processing.parallel.helpers import run_chunk, merge_excel_files
+
+
+def augment_data_parallel(
+    paths: PathBundle,
+    orders_to_consider: list[int],
+    generate_full_pages: bool,
+    generate_paragraphs: bool,
+    tasks_only: list[int] | None,
+    lsi: LabelStudioInterface | None = None,
+):
+    lsi = lsi if (lsi is not None) else LabelStudioInterface(paths)
+
+    simplified_tasks = lsi.simplified_tasks
+
+    if tasks_only:
+        tasks_only = [str(i) for i in tasks_only]
+        simplified_tasks = [
+            t for t in simplified_tasks if str(t.get("id")) in tasks_only
+        ]
+
+    all_task_ids = [str(t.get("id")) for t in simplified_tasks]
+
+    # hacemos un shuffle para que no todas las cartas vayan al mismo proceso (son más sencillas)
+    np.random.shuffle(all_task_ids)
+
+    # no podemos usar una definición de función dentro de esta
+    # porque luego multiprocessing no puede mandar esa información (cosas de implementación)
+    run_chunk_configured = partial(
+        run_chunk,
+        paths=paths,
+        orders_to_consider=orders_to_consider,
+        generate_full_pages=generate_full_pages,
+        generate_paragraphs=generate_paragraphs,
+    )
+
+    num_processes = min(len(all_task_ids), max(1, int(cpu_count() * 0.8)))
+
+    # Calculamos el tamaño de cada tanda
+    chunk_size = int(np.ceil(len(all_task_ids) / num_processes))
+    chunks = []
+
+    for i in range(num_processes):
+        subset = all_task_ids[i * chunk_size : (i + 1) * chunk_size]
+
+        if subset:
+            chunks.append((subset, i))
+
+    print(f"Procesando {len(all_task_ids)} tareas usando {num_processes} núcleos.")
+
+    with Pool(processes=num_processes) as pool:
+        # creamos tantos procesos como hayamos indicado
+
+        # le adjudicamos a nuestros procesos las tareas (correr run_chunk en cada elemento de chunk)
+        results = pool.map(run_chunk_configured, chunks)
+
+    print("Procesado terminado, combinando excels...")
+
+    # 6. Combine Results
+    output_excel_name = "pairs.xlsx"
+    merge_excel_files(
+        base_name="pairs_part", output_name=output_excel_name, paths=paths
+    )
---
+++ cropgen/tests/processing/test_audit_annotations.py
@@ -0,0 +1,14 @@
+from cropgen.processing.AnnotatedPage import AnnotatedPage
+from PIL import Image
+
+
+def test_audit_annotations(paths, ls_url, ls_token, lsi):
+
+    for task in lsi.simplified_tasks[:50] + lsi.simplified_tasks[-50:]:
+        image_path = paths.get_image_path_from_task(task)
+        image = Image.open(image_path)
+        for ann in task["annotations"]:
+            ann = AnnotatedPage(ann, image, usernames_LS=lsi.usernames)
+            ann.assert_pairing()  # esto ya se llama dentro del AnnotatedPage.__init__(), pero por asegurar
+
+    assert AnnotatedPage.n_annotation_errors == 0
