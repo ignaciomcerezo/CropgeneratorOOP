1e1d318a0178cb32b789e1a1e414e95b901aedc6
Nacho <pambignacio8@gmail.com>
2026-06-29 12:43:47 +0200

    0.97 -> 0.95

--- cropgen/splitter/generation/get_dataset.py
+++ cropgen/splitter/generation/get_dataset.py
@@ -10,7 +10,7 @@ from cropgen.splitter.generation.generate_generator import (
 def get_datasets(
     pdi: PairsDataInterface,
     orders_to_split_with: list[int],
-    p: float = 0.97,
+    p: float = 0.95,
     augment=True,
 ) -> tuple[Dataset, Dataset]:
     """
